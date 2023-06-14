1e2dfbe72278cf1a4a15f62a801d596fd8237501
ShufyBungaz <auliyashufy64@gmail.com>
2023-06-14 11:30:25 +0800

    Membuat class Mahasiswa  dan mahasiswaBeraksi

--- src/universitas/mahasiswa.java
+++ src/universitas/mahasiswa.java
@@ -1,14 +1,36 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package universitas;
 
-/**
- *
- * @author USER
- */
 public class mahasiswa {
+ String npm,nama,prodi;
+  public mahasiswa(String npm, String nama, String prodi) {
+        this.npm = npm;
+        this.nama = nama;
+        this.prodi = prodi;
+    }
+
+    public String getNpm() {
+        return npm;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public String getProdi() {
+        return prodi;
+    }
+
+    public void setNpm(String npm) {
+        this.npm = npm;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public void setProdi(String prodi) {
+        this.prodi = prodi;
+    }
     
 }
