898db1c55c29a4b80199e2ae1d8529c0d7f5d12c
ShufyBungaz <auliyashufy64@gmail.com>
2023-06-14 11:34:19 +0800

    Menjalankan program

--- src/universitas/mahasiswaBeraksi.java
+++ src/universitas/mahasiswaBeraksi.java
@@ -1,14 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package universitas;
 
-/**
- *
- * @author USER
- */
 public class mahasiswaBeraksi {
-    
+ public static void main(String[] args) {
+        mahasiswa data = new mahasiswa("2110010091","Muhammad Shufy Auliya","TI");
+        
+        System.out.println("NPM ="+data.getNpm());
+        System.out.println("Nama ="+data.getNama());
+        System.out.println("Prodi ="+data.getProdi());
+        
+    }   
 }
